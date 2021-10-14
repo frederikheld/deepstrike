6f941bac329d867ebde6f5b09c54b6f6935f9fb8
Frederik Held <git@frederikheld.de>
2021-10-14 15:33:26 +0200

    Fixes export in index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use strict'
 
 module.exports = {
-  patchObject: require('./lib/object.patch'),
+  patchObject: require('./lib/patch-object'),
   patchObjectInPlace: require('./lib/patch-object-in-place')
 }
