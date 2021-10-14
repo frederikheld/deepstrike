17ab8db4f5641a38b67dca23a1bd2f6f6281a814
Frederik S. Held <git@frederikheld.de>
2021-10-14 16:00:05 +0200

    Adds new function buildNestedObject

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict'
 
 module.exports = {
+  buildNestedObject: require('./lib/build-nested-object'),
   patchObject: require('./lib/patch-object'),
   patchObjectInPlace: require('./lib/patch-object-in-place')
 }
