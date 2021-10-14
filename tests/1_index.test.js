f589904ae976a19932eec9bfcbca0e848ee375ed
Frederik S. Held <git@frederikheld.de>
2021-10-14 15:59:36 +0200

    Improves order of test execution

--- tests/1_index.test.js
+++ tests/1_index.test.js
@@ -4,12 +4,18 @@ const chai = require('chai')
 chai.should()
 const expect = chai.expect
 
+const buildNestedObject = require('../lib/build-nested-object')
 const patchObject = require('../lib/patch-object')
 const patchObjectInPlace = require('../lib/patch-object-in-place')
 
 const ö = require('../index')
 
 describe('module', function () {
+  it('exports the function `buildNestedObject`', function () {
+    expect(ö.buildNestedObject).to.equal(buildNestedObject)
+    expect(typeof ö.buildNestedObject).equals('function')
+  })
+
   it('exports the function `patchObject`', function () {
     expect(ö.patchObject).to.equal(patchObject)
     expect(typeof ö.patchObject).equals('function')
