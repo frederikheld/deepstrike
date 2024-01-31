cf3f4afd437529ac5f5b6ccaea8ecfd8d996b9ce
Frederik S. Held <kontakt@frederikheld.de>
2024-01-31 13:51:21 +0100

    Implements kebabCaseToCamelCase

--- tests/kebab-case-to-camel-case.test.js
+++ tests/kebab-case-to-camel-case.test.js
@@ -10,4 +10,19 @@ describe('kebabCaseToCamelCase', function () {
   it('exports the function kebabCaseToCamelCase', function () {
     expect(kebabCaseToCamelCase.name).to.equal('kebabCaseToCamelCase')
   })
+
+  it('turns the given string in kebab-case into a return value in camelCase', function () {
+    const samples = [
+      { in: 'kebab-case', out: 'kebabCase' },
+      { in: 'something-else', out: 'somethingElse' },
+      { in: 'kebab-case-multi', out: 'kebabCaseMulti' },
+      { in: 'some-more-multi', out: 'someMoreMulti' },
+      { in: 'even-more-concatenated-words', out: 'evenMoreConcatenatedWords' },
+      { in: 'kebab', out: 'kebab' }
+    ]
+
+    samples.forEach((item) => {
+      expect(kebabCaseToCamelCase(item.in)).to.equal(item.out)
+    })
+  })
 })
