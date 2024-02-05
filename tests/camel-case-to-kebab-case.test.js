43b3a110e98a2c4ad48a14773102f009c759cb1a
Frederik S. Held <kontakt@frederikheld.de>
2024-02-05 02:30:30 +0100

    Adds edge case handling

--- tests/camel-case-to-kebab-case.test.js
+++ tests/camel-case-to-kebab-case.test.js
@@ -20,12 +20,29 @@ describe('camelCaseToKebabCase', function () {
       { in: 'evenMoreConcatenatedWords', out: 'even-more-concatenated-words' },
       { in: 'camel', out: 'camel' },
       { in: 'c', out: 'c' },
-      { in: 'cAMeL', out: 'c-a-me-l' },
-      { in: '', out: '' }
+      { in: 'cAMeL', out: 'c-a-me-l' }
     ]
 
     samples.forEach((item) => {
       expect(camelCaseToKebabCase(item.in)).to.equal(item.out)
+    })
+  })
+
+  it('returns an empty string if an empty string was given', function () {
+    expect(camelCaseToKebabCase('')).to.equal('')
+  })
+
+  it('throws an error if the input string is not correct camelCase', function () {
+    const samples = [
+      'PascalCase',
+      '12something',
+      ' '
+    ]
+
+    samples.forEach((item) => {
+      expect(() => {
+        camelCaseToKebabCase(item)
+      }).to.throw(TypeError, 'Input is not valid camelCase!')
     })
   })
 })
